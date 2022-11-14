df48b35d49784029374af2e9c6c1b00ec2c79aee
sergiomm84 <sergiomonterrubio@outlook.com>
2022-11-14 14:01:07 -0600

    TestNG 1

---
+++ src/Demo/FirstClass.java
@@ -0,0 +1,19 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class FirstClass {
+	@Test
+	public void FirstTest() {
+		System.out.println("Inside function 1");
+		
+	}
+	
+	@Test
+	public void SecondTest() {
+		
+		System.out.println("Inside function 2");
+		
+	}
+
+}
